88ef0fbaa0be0e6eccddf2346096d96c192cd475
walker0625 <jeonminwoo0625@gmail.com>
2025-10-28 21:54:08 +0900

    feat : path file and persona file separate

---
+++ util/path.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+
+_PATH_FILE = Path(__file__).resolve()
+
+PROJECT_ROOT = _PATH_FILE.parent.parent
+
+# === Root 하위 디렉토리 ===
+BACKEND_DIR = PROJECT_ROOT / "backend"
+FRONTEND_DIR = PROJECT_ROOT / "frontend"
+UTIL_DIR = PROJECT_ROOT / "util"
+RAW_DATA_DIR = PROJECT_ROOT / "raw_data"
+
+# === Backend 하위 디렉토리 ===
+ANALYSIS_DIR = BACKEND_DIR / "analysis"
+DATA_DIR = BACKEND_DIR / "raw_data"
+PROMPT_DIR = BACKEND_DIR / "prompt"
+
+# === RawData 하위 디렉토리 ===
+PDF_DIR = RAW_DATA_DIR / "pdf"
